d76bc19f59a51eb3168b9187c5611f8756661d6f
Rebecca Kawa <reb.kawa@gmail.com>
2019-05-12 10:22:38 +0300

    MVP

--- src/App.js
+++ src/App.js
@@ -6,82 +6,64 @@ import axios from "axios"
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-
     //   //const movies = 
     this.state = {
       movie: "",
       year: "",
-
-    }
-  } 
-    handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(this.state.movie);
-      console.log(this.state.year);
-
-      // how to get the informations in the url????
-
-      var APIurl = 'http://www.omdbapi.com/movie=' + this.state.movie + '&year=' + this.state.year + '?i=tt3896198&apikey=c3901c76'
-      console.log(APIurl);
-
-      axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=c3901c76')
-        .then(function (response) {
-
-          console.log(response.data)
-          var results=response.results
-           var movieList = [];
-
-           results.forEach((movie) => {
-            var movie = <Results movie={movie}/>
-
-            movieList.push(movie)
-            console.log(movieList);
-            
-
-           })
-
-           this.setState({list: movieList })
-
-          
-        })
-        .catch(function (e) {
-          console.log("ERROR ", e);
-        })
-      
-  }
-
-      
-    render() {
-      return (
-        <div>
-          <form className='form' onSubmit={(e) => {
-            this.handleSubmit(e)
-          }}>
-            <label>
-              
-          </label>
-            <input id='movieInput' className='input' required type="text" placeholder='Enter Movie Name' onChange={(e) => this.setState({
-              movie: e.target.value
-            })} value={this.state.movie}></input>
-            <label>
-              
-          </label>
-            <input id='yearInput' className='input' type="number" placeholder='Enter Movie Year' min='1900' onChange={(e) => this.setState({
-              year: e.target.value
-            })} value={this.state.year}></input>
-            <button className='button' type="submit" >Submit</button>
-            <button className='button' >Clear</button>
-          </form>
-          <Results/>
-
-        </div>
-
-      );
+      movieObj: null
     }
   }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.movie);
+    console.log(this.state.year);
+
+    // how to get the informations in the url????
+
+    var APIurl = `http://www.omdbapi.com/?apikey=c3901c76&t=${this.state.movie}&y=${this.state.year}`
+    console.log(APIurl);
+    let that = this;
+    axios.get(APIurl)
+      .then(function (response) {
+        that.setState({
+          movieObj: response.data
+        })
+      })
+      .catch(function (e) {
+        console.log("ERROR ", e);
+      })
+
+  }
+  render() {
+    return (
+      <div>
+        <form className='form' onSubmit={(e) => {
+          this.handleSubmit(e)
+        }}>
+          <label>
+
+          </label>
+          <input id='movieInput' className='input' required type="text" placeholder='Enter Movie Name' onChange={(e) => this.setState({
+            movie: e.target.value
+          })} value={this.state.movie}></input>
+          <label>
+
+          </label>
+          <input id='yearInput' className='input' type="number" placeholder='Enter Movie Year' min='1900' onChange={(e) => this.setState({
+            year: e.target.value
+          })} value={this.state.year}></input>
+          <button className='button' type="submit" >Submit</button>
+          <button className='button' >Clear</button>
+        </form>
+        <Results movie={this.state.movieObj} />
+
+      </div>
+
+    );
+  }
+}
 
 
 
 
-  export default App;
+export default App;
--- src/components/Results.js
+++ src/components/Results.js
@@ -3,16 +3,31 @@ import React from 'react';
 class Results extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return (  
+    render() {
+        return (
             <div className='resultDiv'>
-                <img src=''></img>
-                <h1>{this.props.movie}</h1>
+                {this.props.movie ?
+                    <div>
+                        <img src=''></img>
+                        <h1>Title: {this.props.movie.Title}</h1>
+                        <div>Rate: {this.props.movie.Rated}</div>
+                        <div>Year: {this.props.movie.Year}</div>
+                        <div>Vote count: {this.props.movie.imdbVotes}</div>
+                        <div>Length: {this.props.movie.Runtime}</div>
+                        <div>Country: {this.props.movie.Country}</div>
+                        <div>Actors: {this.props.movie.Actors}</div>
+                        <div>Director(s): {this.props.movie.Director}</div>
+                        <div>Plot: {this.props.movie.Plot}</div>
+                        <div>Website: {this.props.movie.Website}</div>
+                    </div>
+                    :
+                    <h1>No movie to display</h1>
+                }
             </div>
         );
     }
 }
- 
+
 export default Results;
